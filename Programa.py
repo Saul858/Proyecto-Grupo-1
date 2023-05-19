67e0e8973a67d4f398e9840732a981e39707ec41
Bryan-3105 <73322852+Bryan-3105@users.noreply.github.com>
2023-05-19 09:32:36 -0600

    carga

--- Programa.py
+++ Programa.py
@@ -30,7 +30,7 @@ def importCSV():
     path = entry1.get()
     df = pd.read_csv(path, sep = ';', decimal = ',')
     print(df)
-    df.to_pickle(r'C:\Users\bryan\OneDrive\Documentos1\Proyecto GCS\df_importado.plk')
+    df.to_pickle(r'C:\Users\brojas\OneDrive - Coopeservidores R.L\Descargas\df_importado.plk')
 
 # SAVE df to 
 
@@ -45,7 +45,7 @@ root.mainloop()
 app = dash.Dash()
 application = app.server
 # read the pickle file
-df= pd.read_pickle(r'C:\Users\bryan\OneDrive\Documentos1\Proyecto GCS\df_importado.plk')
+df= pd.read_pickle(r'C:\Users\brojas\OneDrive - Coopeservidores R.L\Descargas\df_importado.plk')
 df.head()
 app.layout = html.Div([
     dcc.Dropdown(
